9fb0cb1320326653b33085c290a977d4e1dcab66
Vipul Shrivastav <vipulmpgi@gmail.com>
2026-09-17 23:20:42 +0530

    Added Vercel deployment configuration and serverless handler

--- server/server.js
+++ server/server.js
@@ -35,3 +35,5 @@ app.listen(PORT, () => {
   console.log(`📡 API Base URL: http://localhost:${PORT}/api`);
   console.log(`===================================================`);
 });
+
+module.exports = app;
